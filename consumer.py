405c8418a29a955b9971fcc79414c892ae1c3231
LovisaLugnegard <lovisa.lugnegard@gmail.com>
2017-10-17 07:44:40 +0000

    trying to recv image data as a stream

--- consumer.py
+++ consumer.py
@@ -10,27 +10,37 @@ def main():
                              value_deserializer = lambda m: m.decode('ascii'))
 
     consumer.subscribe(topics=['test'])
+#    print("in main")
+    print('hohoho')
+ #   return ('hejhej')
+#    return Response(events(),
+ #                   mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    def events():
-       for message in consumer:
+
+
+def events():
+   for message in consumer:
  #         if message is not None:
   #            result='{} {} '.format(message.value, message.offset) #.append('hello') #str(message.value).decode('utf-8'))  # <--- here (str)
    #       yield result
-          f = open("test.txt","a") #opens file with name of "test.txt"
+      print(message.offset)
+#          yield (b'--frame\r\n'
+#                 b'Content-Type:image/png\r\n\r\n' + message.value + b'\r\n\r\r')
 
-          f.write("\n {} offset: {} ".format(message.value, message.offset))
+      f = open("test.txt","a") #opens file with name of "test.txt"
+      f.write("\n offset: {} ".format(message.offset))
 
-          f.close()
+      f.close()
 
          # print(message.value)
 #    for message in consumer:
         # This will wait and print messages as they become available
        # print('Offset: %s' % message.offset)
  #       return 'Offset: %s' % message.offset
-    return Response(events())
+   return Response(events())
 
 if __name__ == "__main__":
    # index()
     main()
- #  app.run(debug=True)
+#    app.run(debug=True)
 
