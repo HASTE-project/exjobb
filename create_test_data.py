7f5868bb8eced6aac640f005fe48c80c34ed35d2
LovisaLugnegard <lovisa.lugnegard@gmail.com>
2017-11-13 10:13:18 +0100

    method to create test data sets of different sizes

--- create_test_data.py
+++ create_test_data.py
@@ -3,38 +3,52 @@
 import os
 import cv2
 import numpy as np
+import shutil
 from skimage.measure import block_reduce
 
 
-def create_test_images(file_path, binning):
-    files = os.listdir(file_path)
+def create_test_images(source, dest, binning):
+    files = os.listdir(source)
     original_size = []
     size_after_binning = []
-    if not os.path.exists(file_path + str(binning)):
-        os.mkdir(file_path + str(binning))
+    if not os.path.exists(dest + str(binning)):
+        os.mkdir(dest + str(binning))
     for file in files:
-        if os.path.isfile(file_path + file):
-            img = cv2.imread(file_path + file, -1)
+        if os.path.isfile(source + file):
+            img = cv2.imread(source + file, -1)
             binned_img = block_reduce(img, block_size=(binning, binning), func=np.sum)
             binned_img = binned_img.astype(np.uint16)
-            cv2.imwrite(file_path + str(binning) + "/binned_" + file, binned_img)
-            original_size.append(os.path.getsize(file_path + file))
-            size_after_binning.append(os.path.getsize(file_path + str(binning) + "/binned_" + file))
+            cv2.imwrite(dest + str(binning) + "/binned_" + file, binned_img)
+            for i in range(5):
+                shutil.copy2(dest + str(binning) + "/binned_" + file,
+                             dest + str(binning) + "/binned_" + str(i) + file)
+            original_size.append(os.path.getsize(source + file))
+            size_after_binning.append(os.path.getsize(dest + str(binning) + "/binned_" + file))
+            break
         else:
             pass
+
     sum_org_size = sum(list(original_size))
     sum_size_after_bin = sum(size_after_binning)
-    print(len(original_size))
+
     try:
+        fo = open("file_information.txt", "a")
+        fo.write("\n Binning: {} ".format(binning))
+        fo.write("\n sum_size_after_bin/sum_org_size: {}".format(sum_size_after_bin/sum_org_size))
+        fo.write("\n original_size: {}".format(original_size))
+        fo.write("\n size_after_binning: {}".format(size_after_binning))
+        fo.write("\n")
+        fo.close()
         print(sum_size_after_bin/sum_org_size)
     except ZeroDivisionError:
         print(sum_org_size)
         print(sum_size_after_bin)
 
 
-create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",1)
-create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",2)
-create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",3)
-create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",4)
-create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",5)
-create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",6)
+create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",
+                   "D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/", 1)
+# create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",2)
+# create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",3)
+# create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",4)
+# create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",5)
+# create_test_images("D:\Bibliotek\Documents\Exjobb\Bilder-20171019T090142Z-001\Bilder/",6)
