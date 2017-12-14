b3758faff9c85ea9880b3458dada6a4cd83068e5
Ben Blamey <blamey.ben@gmail.com>
2017-12-14 07:10:57 +0100

    more examples

---
+++ examples/example_simulator_no_flask_dummy_set.py
@@ -0,0 +1,4 @@
+
+import simulator_no_flask
+dir = './test-images'
+simulator_no_flask.get_files(dir, 1, 1, ["1"], "yes")
--- examples/example_simulator_no_flask_from_volume.py
+++ examples/example_simulator_no_flask_from_volume.py
@@ -1,5 +1,4 @@
 
 import simulator_no_flask
 dir = '/mnt/volume2/testDatasets/0'
-dir = './test-images'
 simulator_no_flask.get_files(dir, 1, 1, ["1"], "yes")
