a90351f451d9147aae7fd76d0a4cd1b2fc4be9b3
Ben Blamey <blamey.ben@gmail.com>
2018-01-03 10:32:49 +0100

    harmonicIO integration

--- example_simulator_no_flask_from_volume.py
+++ example_simulator_no_flask_from_volume.py
@@ -1,3 +1,16 @@
 import simulator_no_flask
 dir = '/mnt/volume2/testDatasets/0'
-simulator_no_flask.get_files(dir, 0.01, 1, ["1"], "yes")
+
+# This pipeline was for ben to test simulator integration
+hio_config_ben_test = {'master_host': '130.239.81.84',
+                       'master_port': 8080,
+                       'container_name': 'hakanwie/test:batch_hist2',
+                       'container_os': 'ubuntu'}
+
+# This is the production pipeline
+hio_config_hokan = {'master_host': '130.239.81.126',
+                    'master_port': 8080,
+                    'container_name': 'hakanwie/test:batch_hist2',
+                    'container_os': 'ubuntu'}
+
+simulator_no_flask.get_files(dir, 0.01, 1, ["1"], "yes", hio_config=hio_config_hokan, stream_id_tag='test_set')
