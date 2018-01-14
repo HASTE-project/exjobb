9d2aeb4201eab23d3c96a28fefa9beca72c6876c
LovisaLugnegard <lovisa.lugnegard@gmail.com>
2018-01-14 21:53:58 +0000

    new test results

--- myvariables.py
+++ myvariables.py
@@ -5,6 +5,6 @@ File where ip-address can be set globally
 #kafka_server = "130.239.81.54"
 kafka_server = "192.168.1.6"
 
-topic = "part30"
+topic = "part25"
 
 max_msg_size = 10000000 # max size for Kafka msg [B]
